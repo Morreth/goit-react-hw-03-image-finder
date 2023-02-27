36c1a34b46cc5924bcb51aeda55da26e3edd6f63
Olha Brylova <47885094+Morreth@users.noreply.github.com>
2023-02-27 15:41:31 +0200

    Update Searchbar.styled.jsx

--- src/components/Searchbar/Searchbar.styled.jsx
+++ src/components/Searchbar/Searchbar.styled.jsx
@@ -15,7 +15,7 @@ export const SearchbarHeader = styled.header`
   padding-bottom: 12px;
   margin-bottom: 20px;
   color: #fff;
-  background-color: tyle;
+  background-color: #3f51b5;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 `;
