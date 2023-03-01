3a47cc9cb3cce9f7281bee3a1875c170d995032b
Olha Brylova <47885094+Morreth@users.noreply.github.com>
2023-03-01 20:30:36 +0200

    button

--- src/components/LoadMore/LoadMore.jsx
+++ src/components/LoadMore/LoadMore.jsx
@@ -2,16 +2,18 @@ import PropTypes from 'prop-types';
 import { LoadMoreBtn } from './LoadMore.styled';
 
 const LoadMore = ({ onClick, isLoading }) => {
+  if(isLoading) { return null; }
+  else {
   return (
-    <LoadMoreBtn type="button" onClick={onClick} disabled={isLoading}>
+    <LoadMoreBtn type="button" onClick={onClick}>
       Load More
     </LoadMoreBtn>
   );
-};
+}};
 
 LoadMore.propTypes = {
   onClick: PropTypes.func,
-  isLoading: propTypes.bool,
+  isLoading: PropTypes.bool,
 };
 
 export default LoadMore;
